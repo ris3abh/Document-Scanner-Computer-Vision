ddf80bc8ba22cbd2939d40248d06af1ea5d35209
Niki Patel <np873@drexel.edu>
2023-06-11 20:41:12 -0400

    Add files via upload

---
+++ code/Scanner.m
@@ -0,0 +1,311 @@
+clear;
+close all;
+
+image = imread('image.jpeg');
+grayImage = rgb2gray(image);
+
+plotTheStatistics(grayImage, 4, image)
+
+function plotTheStatistics(grayImage, sigma, image)
+    k = 2 * ceil(3 * sigma) + 1; 
+    [smoothImage, ~] = edgeDetectionOfImage(grayImage, k, sigma);
+    
+    edges = findOptimumEdges(smoothImage);
+    detectedEdges = removeBackgroundNoise(edges);
+    smoothImage = detectedEdges;
+
+    [houghTransformLine, rRange, thetaRange] = houghTransformForLine(detectedEdges);
+
+    [numberOfLines, relevantLines] = relevantLineIdentification(houghTransformLine);
+
+    subplot(2,2,1);
+    imshow(image);
+    title('Original Image');
+
+    subplot(2,2,2);
+    imshow(smoothImage);
+    title('Edge Detection');
+
+    subplot(2,2,3);
+    imshow(houghTransformLine',[]);
+    title('Hough Transform for Line');
+
+    subplot(2,2,4);
+    imshow(image);
+    hold on;
+    for i = 1:numberOfLines
+        theta = deg2rad(thetaRange(relevantLines(i, 1)));
+        r = rRange(relevantLines(i, 2));
+        x = 1:size(image, 2);
+        y = (r - x * cos(theta)) / sin(theta);
+        plot(x, y, 'Color', 'r');
+    end
+
+    % Get corner points
+    imageSize = size(image);
+    cornerPoints = getCornerPoints(numberOfLines, relevantLines, rRange, thetaRange, imageSize);
+    
+    % Plot lines connecting corner points
+    plot(cornerPoints(1, 1), cornerPoints(1, 2), 'g*', 'MarkerSize', 10)
+    plot(cornerPoints(2, 1), cornerPoints(2, 2), 'g*', 'MarkerSize', 10)
+    plot(cornerPoints(3, 1), cornerPoints(3, 2), 'g*', 'MarkerSize', 10)
+    plot(cornerPoints(4, 1), cornerPoints(4, 2), 'g*', 'MarkerSize', 10)
+    
+    text(cornerPoints(2, 1), cornerPoints(2, 2), 'Top Right', 'Color', 'g', 'VerticalAlignment', 'bottom', 'HorizontalAlignment', 'left');
+    text(cornerPoints(1, 1), cornerPoints(1, 2), 'Top Left', 'Color', 'g', 'VerticalAlignment', 'bottom', 'HorizontalAlignment', 'right');
+    text(cornerPoints(3, 1), cornerPoints(3, 2), 'Bottom Right', 'Color', 'g', 'VerticalAlignment', 'top', 'HorizontalAlignment', 'left');
+    text(cornerPoints(4, 1), cornerPoints(4, 2), 'Bottom Left', 'Color', 'g', 'VerticalAlignment', 'top', 'HorizontalAlignment', 'right');
+    hold off;
+
+    rectifiedImage = rectifyImage(image, cornerPoints);
+    figure;
+    imshow(rectifiedImage);
+
+end
+
+function edges = findOptimumEdges(image)
+    bestScore = 0;
+    bestEdges = [];
+    for k = 3:2:30
+        for sigma = 0.5:0.1:3.0
+            [~, edgePixels] = edgeDetectionOfImage(image, k, sigma);
+            score = sum(edgePixels(:));
+            if score > bestScore
+                bestScore = score;
+                bestEdges = edgePixels;
+            end
+        end
+    end
+    edges = bestEdges;
+end
+
+function [smoothImage, edgePixels] = edgeDetectionOfImage(image, k, sigma)
+    kernel = zeros(k, k);
+    center = (k + 1) / 2;
+    for i = 1:k
+        for j = 1:k
+            distance = ((i - center)^2 + (j - center)^2) / (2 * sigma^2);
+            kernel(i, j) = exp(-distance);
+        end
+    end
+    kernel = kernel / sum(kernel(:)); 
+    smoothImage = conv2(image, kernel, 'same'); 
+    dx = zeros(size(smoothImage));
+    dy = zeros(size(smoothImage));
+    for i = 2:size(smoothImage, 1) - 1
+        for j = 2:size(smoothImage, 2) - 1
+            dx(i,j) = (smoothImage(i + 1, j) - smoothImage(i - 1, j))/2;
+            dy(i,j) = (smoothImage(i, j + 1) - smoothImage(i, j - 1))/2;
+        end
+    end
+    magnitude = sqrt(dx.^2 + dy.^2);
+    threshold = 0.4 * max(magnitude(:)); 
+    edgePixels = magnitude > threshold;
+end
+
+function detectedEdges = removeBackgroundNoise(edgePixels)
+    binarizedImage = edgePixels > 0;
+    labeledImage = zeros(size(binarizedImage));
+    label = 1;
+    [height, width] = size(binarizedImage);
+    for i = 1:height
+        for j = 1:width
+            if binarizedImage(i, j) == 1 && labeledImage(i, j) == 0
+                labeledImage = depthFirstSearch(binarizedImage, labeledImage, label, i, j);
+                label = label + 1;
+            end
+        end
+    end
+    maxPixels = 0;
+    maxLabel = 0;
+    for label = 1:(label - 1)
+        numPixels = sum(labeledImage(:) == label);
+        if numPixels > maxPixels
+            maxPixels = numPixels;
+            maxLabel = label;
+        end
+    end
+    detectedEdges = (labeledImage == maxLabel);
+end
+
+function labeledImage = depthFirstSearch(binarizedImage, labeledImage, label, i, j)
+    [height, width] = size(binarizedImage);
+    stackSize = height * width;
+    stack = zeros(stackSize, 2); 
+
+    stackIndex = 1; 
+    stack(stackIndex, :) = [i, j];  
+
+    while stackIndex > 0
+        currPos = stack(stackIndex, :);
+        stackIndex = stackIndex - 1; 
+
+        if currPos(1) >= 1 && currPos(1) <= height && currPos(2) >= 1 && currPos(2) <= width
+            if binarizedImage(currPos(1), currPos(2)) == 1 && labeledImage(currPos(1), currPos(2)) == 0
+                labeledImage(currPos(1), currPos(2)) = label;
+                stackIndex = stackIndex + 1;
+                stack(stackIndex, :) = [currPos(1)-1, currPos(2)];  
+                stackIndex = stackIndex + 1;
+                stack(stackIndex, :) = [currPos(1)+1, currPos(2)];
+                stackIndex = stackIndex + 1;
+                stack(stackIndex, :) = [currPos(1), currPos(2)-1];
+                stackIndex = stackIndex + 1;
+                stack(stackIndex, :) = [currPos(1), currPos(2)+1];
+            end
+        end
+    end
+end
+
+
+function [houghTransformLine, rRange, thetaRange] = houghTransformForLine(detectedEdges)
+    thetaRange = 0:360;
+    w = size(detectedEdges,2);
+    h = size(detectedEdges,1);
+    rMax = round(sqrt(w^2 + h^2));
+    rRange = -rMax:rMax;
+    
+    houghTransformLine = zeros(length(rRange), length(thetaRange));
+    
+    [y,x] = find(detectedEdges);
+    edges = length(x);
+    for i = 1:edges
+        for j = 1:length(thetaRange)
+            r = round(x(i)*cosd(thetaRange(j)) + y(i)*sind(thetaRange(j))) + rMax + 1;
+            % Increment the corresponding bin in the Hough Transform array
+            houghTransformLine(r,j) = houghTransformLine(r,j) + 1;
+        end
+    end
+end
+
+function localMaxima = findLocalMaxima(houghTransformLine)
+    [rows, cols] = size(houghTransformLine);
+    localMaxima = false(size(houghTransformLine));
+
+    for i = 2:rows-1
+        for j = 2:cols-1
+            pixelValue = houghTransformLine(i, j);
+
+            % Check if the pixel is greater than its neighbors
+            if pixelValue > houghTransformLine(i-1, j-1) && ...
+               pixelValue > houghTransformLine(i-1, j) && ...
+               pixelValue > houghTransformLine(i-1, j+1) && ...
+               pixelValue > houghTransformLine(i, j-1) && ...
+               pixelValue > houghTransformLine(i, j+1) && ...
+               pixelValue > houghTransformLine(i+1, j-1) && ...
+               pixelValue > houghTransformLine(i+1, j) && ...
+               pixelValue > houghTransformLine(i+1, j+1)
+                localMaxima(i, j) = true;
+            end
+        end
+    end
+end
+
+function [numberOfLines, relevantLines] = relevantLineIdentification(houghTransformLine)
+    threshold = 0.65 * max(houghTransformLine(:));
+    [h, w] = size(houghTransformLine);
+    localMaxima = findLocalMaxima(houghTransformLine);
+    linesAboveThreshold = houghTransformLine >= threshold;
+    [rows, cols] = find(localMaxima & (linesAboveThreshold));
+    [~, indices] = sort(houghTransformLine(sub2ind([h, w], rows, cols)), 'descend');
+    rows = rows(indices);
+    cols = cols(indices);
+    numberOfLines =  numel(indices);
+    relevantLines = [cols(1:numberOfLines), rows(1:numberOfLines)];
+end
+
+function cornerPoints = getCornerPoints(numberOfLines, relevantLines, rRange, thetaRange, imageSize)
+    % Compute intersection points of lines
+    cornerPoints = zeros(4, 2);
+    idx = 1;
+
+    for i = 1:numberOfLines-1
+        theta1 = deg2rad(thetaRange(relevantLines(i, 1)));
+        r1 = rRange(relevantLines(i, 2));
+        m1 = -cos(theta1) / sin(theta1);
+        b1 = r1 / sin(theta1);
+
+        for j = i + 1:numberOfLines
+            theta2 = deg2rad(thetaRange(relevantLines(j, 1)));
+            r2 = rRange(relevantLines(j, 2));
+            m2 = -cos(theta2) / sin(theta2);
+            b2 = r2 / sin(theta2);
+
+            xIntersect = (b2 - b1) / (m1 - m2);
+            yIntersect = m1 * xIntersect + b1;
+
+            % Check if intersection point is within image bounds
+            if xIntersect > 0 && xIntersect <= imageSize(2) && yIntersect > 0 && yIntersect <= imageSize(1)
+                cornerPoints(idx, :) = [xIntersect, yIntersect];
+                idx = idx + 1;
+            end
+        end
+    end
+
+    % Find top-left and bottom-right corners
+    cornerSums = sum(cornerPoints, 2);
+    [~, topLeftIndex] = min(cornerSums);
+    [~, bottomRightIndex] = max(cornerSums);
+    
+    % Find top-right and bottom-left corners
+    cornerDiffs = diff(cornerPoints, [], 2);
+    [~, topRightIndex] = min(cornerDiffs);
+    [~, bottomLeftIndex] = max(cornerDiffs);
+    
+    % Get corner points
+    topLeft = cornerPoints(topLeftIndex, :);
+    topRight = cornerPoints(topRightIndex, :);
+    bottomLeft = cornerPoints(bottomLeftIndex, :);
+    bottomRight = cornerPoints(bottomRightIndex, :);
+    
+    % Update cornerPoints array
+    cornerPoints = [topLeft; topRight; bottomRight; bottomLeft];
+end
+
+function rectifiedImage = rectifyImage(inputImage, cornerPoints)
+    paperWidth = 8.5;
+    paperHeight = 11;
+    paperAspectRatio = paperWidth / paperHeight;
+    imageWidth = size(inputImage, 2);
+    imageHeight = size(inputImage, 1);
+    imageAspectRatio = imageWidth / imageHeight;
+    if imageAspectRatio >= paperAspectRatio
+        blankWidth = imageWidth;
+        blankHeight = round(blankWidth / paperAspectRatio);
+    else
+        blankHeight = imageHeight;
+        blankWidth = round(blankHeight * paperAspectRatio);
+    end
+    rectifiedImage = uint8(zeros(blankHeight, blankWidth, 3));
+    sourcePoints = [1, 1; imageWidth, 1; imageWidth, imageHeight; 1, imageHeight];
+    homographyMatrix = computeHomographyMatrix(sourcePoints, cornerPoints);
+    for y = 1:blankHeight
+        for x = 1:blankWidth
+            sourceLocation = homographyTransform([x, y], homographyMatrix);
+            if sourceLocation(1) >= 1 && sourceLocation(1) <= imageWidth && sourceLocation(2) >= 1 && sourceLocation(2) <= imageHeight
+                rectifiedImage(y, x, :) = inputImage(round(sourceLocation(2)), round(sourceLocation(1)), :);
+            end
+        end
+    end
+end
+
+function homographyMatrix = computeHomographyMatrix(sourcePoints, destinationPoints)
+    A = zeros(8, 9);
+    for i = 1:4
+        x = sourcePoints(i, 1);
+        y = sourcePoints(i, 2);
+        u = destinationPoints(i, 1);
+        v = destinationPoints(i, 2);
+        A((i-1)*2+1, :) = [x, y, 1, 0, 0, 0, -u*x, -u*y, -u];
+        A(i*2, :) = [0, 0, 0, x, y, 1, -v*x, -v*y, -v];
+    end
+
+    [~, ~, V] = svd(A);
+    homographyMatrix = reshape(V(:, end), 3, 3)';
+end
+
+function transformedPoint = homographyTransform(point, homographyMatrix)
+    x = point(1);
+    y = point(2);
+    p = homographyMatrix * [x; y; 1];
+    transformedPoint = p(1:2) / p(3);
+end
